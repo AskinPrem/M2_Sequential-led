69b06e3bf0062bc9b49c2f5768f98425aaaf603c
ASKIN P <102133904+AskinPrem@users.noreply.github.com>
2022-04-22 14:54:10 +0530

    Add files via upload

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,27 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 21-04-2022 12:44:26
+ * Author : askin
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB=0xFF;
+	unsigned char z;
+	while(1)
+	{
+		
+		for(z=0;z<255;z++)
+		{
+			PORTB=z;
+			_delay_ms(1000);
+		}
+	}
+	return 0;
+}
+
